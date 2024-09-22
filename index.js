7e3e882b788260ec12c38e87fcdebed7a2723b7b
Arkadipta-Kundu <arkadiptak49@gmail.com>
2024-09-22 20:20:10 +0530

    update index.js and views/index.ejs

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ app.set('view engine', 'ejs');
 // Routes
 app.get('/', (req, res) => {
     res.render('index', { weather: undefined, news: undefined });
+    // res.send("Hello World !");
 });
 
 
 app.post('/', async (req, res) => {
-    const city = req.body.city;
+    const city = req.body.cityName;
     try {
         const weatherResponse = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weatherApiKey}`);
         const newsResponse = await axios.get(`https://newsapi.org/v2/everything?q=${city}&apiKey=${newsApiKey}`);
